c0cf55facba006de214edd6f299431d406ace37b
andrea1632 <andrea.bellucci10@libero.it>
2022-04-22 15:01:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+//import axios from 'axios';
+import "bootstrap"
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css";
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
